70c3a881a28084f67e2ea42316d3502efff315f5
Tipu_Singh <tipu.singh@ayanworks.com>
2024-09-27 19:49:23 +0530

    feat: updated constant for multiplt RP support

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_EXPECTED_ORIGINS = ['http://localhost:3000', 'http://localhost:3001'];
-export const DEFAULT_RP_ID = ['localhost'];
+export const DEFAULT_EXPECTED_ORIGINS = ['http://localhost:3000','http://localhost:3001','http://dev.credebl.id','http://qa.credebl.id','http://credebl.id','https://sandbox.credebl.id'];
+export const DEFAULT_RP_ID = ['localhost','dev.credebl.id','qa.credebl.id','credebl.id'];
 export const TIMEOUT = 60000
 export const RP_NAME = 'CREDEBL'
